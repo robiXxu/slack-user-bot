de8af325fc46537a5eb389dc0cf05ee058c86ba5
Schiriac Robert <schiriac.robert@gmail.com>
2016-12-30 02:29:27 +0200

    removed log & changed default response

--- core/services/spotify.js
+++ core/services/spotify.js
@@ -2,7 +2,6 @@ const _ = require('lodash'),
       spotify = require('spotifyr');
 
 function load(args){
-  console.log(args);
   return new Promise((resolve,reject) => {
     if(args.length){
       //search for track by default
